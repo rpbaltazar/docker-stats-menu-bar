94dff30f703697d8fe7613310461ce95e1cc00eb
Rui Baltazar <rui.p.baltazar@gmail.com>
2021-04-06 09:07:07 +0800

    Initial Commit

---
+++ docker-stats/docker_statsApp.swift
@@ -0,0 +1,17 @@
+//
+//  docker_statsApp.swift
+//  docker-stats
+//
+//  Created by Perx on 6/4/21.
+//
+
+import SwiftUI
+
+@main
+struct docker_statsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
